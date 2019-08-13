37eed6bb1364e21cbcf157f6d471c3dc8ed05566
Plinio Andrade <fisquimco@gmail.com>
2019-08-13 17:36:09 -0300

    Created the Institute Model, View and Controller

---
+++ ChapterOneSolution/ChapterOne/Controllers/InstituteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChapterOne.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterOne.Controllers
+{
+    public class InstituteController : Controller
+    {
+        private static IList<Institute> institutions = new List<Institute>
+        {
+            new Institute { InstituteId = 1, Name = "UniParaná", Address = "Paraná" },
+            new Institute { InstituteId = 2, Name = "UniSanta", Address = "Santa Catarina" },
+            new Institute { InstituteId = 3, Name = "UniSãoPaulo", Address = "São Paulo" },
+            new Institute { InstituteId = 4, Name = "UniSulGrandense", Address = "Rio Grande do Sul" },
+            new Institute { InstituteId = 5, Name = "UniCarioca", Address = "Rio de Janeiro" }
+        };
+        public IActionResult Index()
+        {
+            return View(institutions);
+        }
+    }
+}
---
+++ ChapterOneSolution/ChapterOne/Models/Institute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChapterOne.Models
+{
+    public class Institute
+    {
+        public long? InstituteId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
